d7412f67c70666ba503a3c414a90ee62812be335
Eril <erilharianto@gmail.com>
2018-08-03 12:54:37 +0700

    Ini cuma percobaan

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>E-List Place For Rimend Someting</title>
+  </head>
+  <body>
+    <div class="Head">
+      <head>
+        <h1>E-list</h1>
+      </head>
+    </div>
+  </body>
+</html>
+<?php
+  echo "Hellow world";
+ ?>
